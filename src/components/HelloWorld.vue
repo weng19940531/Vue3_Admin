e4c6e0a7cd89a09df6577e3cb7d246dd8537d3e6
wengzuxing <996675933@qq.com>
2022-05-07 15:02:58 +0800

    feat: element-plus & 样式主题 &  组件自动导入 & Inspect 插件

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,12 @@
 <script setup>
-import { ref } from 'vue'
+import { ElMessage } from 'element-plus'
 
-defineProps({
-  msg: String
-})
-
-const count = ref(0)
+function openMessage() {
+  ElMessage.success('success base 调整为 gray')
+}
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VS Code</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  <el-button type="primary" @click="openMessage">点击测试【unplugin-element-plus】</el-button>
+  <div>测试插件【unplugin-vue-components】</div>
 </template>
-
-<style scoped>
-a {
-  color: #42b983;
-}
-</style>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,34 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import ElementPlus from 'unplugin-element-plus/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import Inspect from 'vite-plugin-inspect'
 
-// https://vitejs.dev/config/
+// reference: https://vitejs.cn/
 export default defineConfig({
-  plugins: [vue()]
+  resolve: {
+    alias: {
+      '@/': `${resolve(__dirname, 'src')}/`
+    },
+    extensions: ['.js', '.vue']
+  },
+  plugins: [
+    vue(),
+    ElementPlus({ useSource: true }),
+    Components({
+      dirs: ['src/components'],
+      resolvers: [ElementPlusResolver({ importStyle: 'sass' })],
+      directoryAsNamespace: true
+    }),
+    Inspect()
+  ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/styles/element/index.scss" as *;`
+      }
+    }
+  }
 })
