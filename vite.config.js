094644c0140a63e9143d057bb6323bbd63092cdc
wzx <996675933@qq.com>
2022-05-08 23:48:34 +0800

    feat: html template

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
+// reference: https://vitejs.cn/
+
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import ElementPlus from 'unplugin-element-plus/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { createHtmlPlugin } from 'vite-plugin-html'
+import { title } from './src/app.config'
 import Inspect from 'vite-plugin-inspect'
 
-// reference: https://vitejs.cn/
 export default defineConfig({
   resolve: {
     alias: {
@@ -21,6 +24,16 @@ export default defineConfig({
       dirs: ['src/components'],
       resolvers: [ElementPlusResolver({ importStyle: 'sass' })],
       directoryAsNamespace: true
+    }),
+    createHtmlPlugin({
+      minify: true,
+      entry: '/src/main.js',
+      template: '/index.html',
+      inject: {
+        data: {
+          title
+        }
+      }
     }),
     Inspect()
   ],
